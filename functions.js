262912fc6cdb78f1eea1c0cebb769c1852339781
zjason25 <90489418+zjason25@users.noreply.github.com>
2024-06-08 18:53:09 -0700

    Add createGrid() to create grid based on dimension input

--- functions.js
+++ functions.js
@@ -1,16 +1,19 @@
-const NUM_ROWS = 4;
+const DEFAULT_NUM_ROWS = 4;
 
 const container = document.querySelector(".container");
 
-for (i = 0; i < NUM_ROWS; i++) {
-  const row = document.createElement("div");
-  row.setAttribute("class", "row");
-  for (j = 0; j < NUM_ROWS; j++) {
-    const sqr = document.createElement("div");
-    sqr.setAttribute("class", "square");
-    row.appendChild(sqr);
+function createGrid(numRows) {
+  for (i = 0; i < numRows; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (j = 0; j < numRows; j++) {
+      const sqr = document.createElement("div");
+      sqr.setAttribute("class", "square");
+      row.appendChild(sqr);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
 }
 
+createGrid(DEFAULT_NUM_ROWS);
 
